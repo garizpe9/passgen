a2795690476a3f6c49886b32d55b3b6aecad2321
Frida <velia.arizpe@gmail.com>
2020-07-20 09:31:46 -0500

    Success

---
+++ script.js
@@ -0,0 +1,74 @@
+ //Variables needed
+     
+ var concatination = "" ;
+ var passwordcomplete = "" ;
+  
+ //array 
+ var sparray = ["!@#%&*?$."]
+ var numarray = ["1234567890"]
+ var dwnarray = ["abcdefghijklmnopqrstuvwxyz"]
+ var uparray = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+
+    //Beginning prompt
+
+ var characters = prompt("Enter number of characters");
+ if (characters === ""  || characters > 128 || characters<3){
+     prompt ("Please enter a number between 3 and 128");
+}
+ 
+ //Character confirm
+ var confirmsp = confirm("Do you want to include special characters?");
+ var confirmup = confirm("Do you want to include capitaized characters?");
+ var confirmlc = confirm("Do you want to include lower case characters?");
+ var confirmnm = confirm("Do you want to include numeric characters?");
+
+
+ //arrays to include
+if (confirmsp !== true){
+  concatination = concatination        
+ }
+else concatination = concatination + sparray
+
+if (confirmup !== true){
+  concatination = concatination
+  }
+else concatination = concatination + uparray
+
+if (confirmlc !== true){
+  concatination = concatination
+  }
+else concatination = concatination + dwnarray
+
+if (confirmnm !== true){
+  concatination = concatination
+  }
+else concatination = concatination + numarray
+
+
+ //math random loop
+
+for (var i=0;i<characters; i++) {
+    var num = Math.floor(Math.random() * concatination.length);
+    var passwordel = (concatination[num]);
+    passwordcomplete = passwordcomplete + passwordel;
+    console.log(passwordcomplete)
+}
+
+var passwordText = document.querySelectorAll("#password")
+password.textContent = passwordcomplete;
+
+
+// // Assignment Code
+
+var generate = document.querySelector("#generate");
+
+generate.addEventListener("click", function() {
+  passwordcomplete = [];
+  for (var i=0;i<characters; i++) {
+    var num = Math.floor(Math.random() * concatination.length);
+    var passwordel = (concatination[num]);
+    passwordcomplete = passwordcomplete + passwordel;
+    console.log(passwordcomplete)
+}
+password.textContent = passwordcomplete;
+});
